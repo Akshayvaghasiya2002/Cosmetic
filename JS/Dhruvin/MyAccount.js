f3ab1129b09a20b41594d8d7c94c72b3f5e02f9f
savanidhruvin <dhruvinsavani9313@gmail.com>
2025-02-11 18:00:10 +0530

    savani11-2

--- JS/Dhruvin/MyAccount.js
+++ JS/Dhruvin/MyAccount.js
@@ -1,3 +1,24 @@
 document.getElementById("calendarIcon").addEventListener("click", function() {
     document.getElementById("datePicker").showPicker();
 });
+
+
+// *********** My Address **********
+function toggleDropdown(icon) {
+    let dropdown = icon.parentElement.parentElement.querySelector(".ds_add_dropdown");
+
+    // Hide all other dropdowns
+    document.querySelectorAll(".ds_add_dropdown").forEach(d => {
+        if (d !== dropdown) d.classList.add("d-none");
+    });
+
+    // Toggle the clicked dropdown
+    dropdown.classList.toggle("d-none");
+}
+
+// Hide dropdowns when clicking outside
+document.addEventListener("click", function (event) {
+    if (!event.target.closest(".fa-ellipsis-vertical")) {
+        document.querySelectorAll(".ds_add_dropdown").forEach(d => d.classList.add("d-none"));
+    }
+});
